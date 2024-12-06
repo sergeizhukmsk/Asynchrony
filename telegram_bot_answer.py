8d0efe8f817676b0d12e98772867fac78c37be78
sergeizhukmsk <sergeizhuk@mail.ru>
2024-12-06 23:12:18 +0300

    Add files via upload

---
+++ telegram_bot_answer.py
@@ -0,0 +1,22 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = '7768492314'
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Привет! Я бот помогающий тебе быстрее стать разработчиком Python.')
+
+
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
